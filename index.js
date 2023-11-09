42ce55a2177e6d1e718b4be021b4d6358b4d548f
Eduard Stroescu <124309370+EduardStroescu@users.noreply.github.com>
2023-11-09 20:29:50 +0200

    Initial Commit

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Server is running!");
-});
-
 app.use("/api/", routes);
 
 const port = process.env.PORT || 3000;
